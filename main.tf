5c23a4c5760805e0ddaf69bd65309d4b0bf752b8
Parker Johansen <johansen.parker@gmail.com>
2021-04-12 01:32:58 -0600

    Add aws provider with region

--- main.tf
+++ main.tf
@@ -10,16 +10,20 @@ terraform {
   }
 }
 
+provider "aws" {
+  region = "us-west-2"
+}
+
 module "spacelift_webhook" {
   source = "https://github.com/auroq/spacelift.git"
 
-  spacelift_stack_id   = spacelift_stack.webhook_tester_trigger.id
+  spacelift_stack_id = spacelift_stack.webhook_tester_trigger.id
 }
 
 resource "spacelift_stack" "webhook_tester_trigger" {
-  name = "Webhook Tester Trigger"
-  description  = "A stack that has changes on every trigger for testing webhooks"
-  
+  name        = "Webhook Tester Trigger"
+  description = "A stack that has changes on every trigger for testing webhooks"
+
   repository = "tf-trigger-always"
   branch     = "main"
   autodeploy = "true"
